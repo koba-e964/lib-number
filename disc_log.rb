e7d8fa048a664cee74bc618d555fae5306c24fd4
8949209744 <8949209744@ci190025m.ecc.u-tokyo.ac.jp>
2013-09-03 15:56:33 +0900

    disc_log.rb
    tries several times

--- disc_log.rb
+++ disc_log.rb
@@ -1,30 +1,40 @@
 load './prime.rb'
-def general_rho(b,a,p) #log_b(a mod p)
+def general_rho(b,a,p)
 	if !mr_prime(p)
         raise Exception 
     end
-    u=p/3
-    v=2*p/3
+    trial=1000
+    for i in 0...trial
+        u=p/3+rand(sqrt(p/2).to_i)
+        v=2*p/3+rand(sqrt(p/2).to_i)
+        res=general_rho_sub(b,a,p,u,v)
+        if res!=nil
+            return res
+        end
+    end
+    nil
+end
+
+def general_rho_sub(b,a,p,u,v) #log_b(a mod p)
 	t0=[1,0,0]
     t1=[1,0,0]
     i=0
     solved=false
+    puts "u="+u.to_s+", v="+v.to_s
     while !solved && i<p
         t0=gr_next(t0,p,u,v,a,b)
         t1=gr_next(t1,p,u,v,a,b)
         t1=gr_next(t1,p,u,v,a,b) #twice
         if(t0[0]==t1[0])
             #debug
-            puts "i="+i.to_s
-            p t0
-            p t1
             m=t0[1]-t1[1]
             n=t1[2]-t0[2]
             mg=gcd(m,p-1)
             if mg==1
                 print (n.to_s+'/'+m.to_s+' mod '+(p-1).to_s)
-                puts '='+((n*ext_gcd(m,p-1)[1])%(p-1)).to_s
+                puts '='+(k=(n*ext_gcd(m,p-1)[1])%(p-1)).to_s
                 solved=true
+                return k
                 break
             else
                 print(n.to_s+'/'+m.to_s+' mod '+(p-1).to_s)
