6b49e6ce1569912c8143e8d0f3d5c84197eb0c61
8949209744 <8949209744@ci190025m.ecc.u-tokyo.ac.jp>
2013-09-03 14:51:50 +0900

    gcd.rb extended successive division
    extended successive division(ext_gcd(a,b))

--- gcd.rb
+++ gcd.rb
@@ -12,3 +12,23 @@ def gcd(a,b)
   end
   a
 end
+
+#returns [g,x] such that g=gcd(a,b) and a*x==g (mod b)
+def ext_gcd(a,b)
+  if b<0
+    b=-b
+  end
+  oldb=b
+  g0=1
+  g1=0
+  while b!=0
+    r=a%b
+    q=a/b
+    a=b
+    b=r
+    g0,g1=g1,g0-q*g1
+    g0%=oldb
+    g1%=oldb
+  end
+  return [a,g0%(oldb/a)]
+end
