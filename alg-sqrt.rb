a5467b7564d7994eeec1ec50d50011cc051d657c
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-09-20 23:31:05 +0900

    alg-sqrt alg_sqrt_base_3
    in progress....

--- alg-sqrt.rb
+++ alg-sqrt.rb
@@ -1,4 +1,6 @@
 load 'field.rb'
+load 'modsqrt.rb'
+
 
 def alg_sqrt_base(k,a,p)  # k:field, a:algebraic integer in k, p:rational prime
 	# (p) must be prime ideal in k.
@@ -16,3 +18,24 @@ def alg_sqrt_base(k,a,p)  # k:field, a:algebraic integer in k, p:rational prime
 	end
 	raise Exception.new("a is not square")
 end
+
+def alg_sqrt_base_3(k,a,p)  # k:field, a:algebraic integer in k, p:rational prime
+	# (p) must be prime ideal in k.
+	if p%4!=3
+		raise Exception.new("p!=3 (mod 4)")
+	end
+	d=k.dim()
+	if d%2!=1
+		raise Exception.new("2|d")
+	end
+	exp=(p**d-1)/(p-1)+1
+	exp/=2
+	na=k.norm(a) #-----------------TODO  k.norm(eta) for arbitrary eta
+	p na
+	b=modsqrt(na,p)
+	beta=k.power_mod(a,exp,p)
+	binv=ext_gcd(b,p)[1]
+	beta=k.mult_mod(beta,[binv],p)
+	return beta
+end
+
