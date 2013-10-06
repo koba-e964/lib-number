e2c40b2fc1955ad06e582e3a7e4e002a852f5704
koba-e964 <silentkiddie-2012@yahoo.co.jp>
2013-10-06 23:39:06 +0900

    gf2n.rb
    operators/prime?

--- gf2n.rb
+++ gf2n.rb
@@ -72,25 +72,103 @@ class GF2Poly
 	def add(ano)
 		return GF2Poly.new(self.val^ano.val)
 	end
+	def +(ano)
+		if(ano.is_a?Integer)
+			ano=GF2Poly.new(ano%2)
+		end
+		return GF2Poly.new(self.val^ano.val)
+	end
 	def mul(ano)
+		return GF2Poly.new(gf2n_mul(self.val,ano.val))
+	end
+	def *(ano)
+		if(ano.is_a?Integer)
+			ano=GF2Poly.new(ano%2)
+		end
 		return GF2Poly.new(gf2n_mul(self.val,ano.val))
 	end
 	def div(ano)
 		res=gf2n_div(self.val,ano.val)
 		return [GF2Poly.new(res[0]),GF2Poly.new(res[1])]
 	end
+	def /(ano)
+		if(ano.is_a?Integer)
+			ano=GF2Poly.new(ano%2)
+		end
+		res=gf2n_div(self.val,ano.val)
+		return [GF2Poly.new(res[0]),GF2Poly.new(res[1])]
+	end
 	def pow(n)
 		return GF2Poly.new(gf2n_pow(self.val,n))
+	end
+	def **(n)
+		return GF2Poly.new(gf2n_pow(self.val,n))
+	end
+	def deg()
+		if self.val==0
+			return nil
+		end
+		return log2(self.val).to_i()
+	end
+	def gcd(ano)
+		x=self
+		y=ano
+		while(y.dim!=nil)
+			r=x.div(y)[1]
+			x=y
+			y=r
+		end
+		return x
+	end
+	def exp(mod)
+		raise Exception unless mod.instance_of? GF2Poly
+		return exponent(self.val,mod.val)
+	end
+	def prime?()
+		return isPrime(self)
 	end
 	def to_s()
 		return format("Poly[%x]",self.val)
 	end
+	def inspect()
+		str=self.class.name+":"
+		d=deg()
+		if d.nil?
+			return str+'0'
+		end
+		nt=0
+		i=d
+		while i>=0
+			if (((val>>i)&1)!=0)
+				if(nt!=0)
+					str+='+'
+				end
+				str+=if i==0 then '1' else 'x^'+i.to_s end
+				nt+=1
+			end
+			i-=1
+		end
+		return str
+	end
+	def self.generate_prime(size)
+		return get_gf2n_prime(size)
+	end
 end
 
 def isPrime(poly)
+	raise Exception unless poly.is_a?GF2Poly
 	val=poly.val
+	if(val==0)
+		return false
+	end
 	sn=log2(val).to_i
+	if sn==0
+		return false
+	end
 	for i in 2..1<<(sn/2+1)
+		if i==val
+			next
+		end
 		if(gf2n_div(val,i)[1]==0)
 			return false
 		end
@@ -111,4 +189,28 @@ def exponent(a,mod)
 		raise Exception("Error:too large exponent")
 	end
 	return cnt
+end
+
+def get_gf2n_prime(deg)
+	trial=0
+	while(trial<deg*10)
+		x=rand(1<<deg)
+		x+=1<<deg
+		x|=1
+		po=GF2Poly.new(x)
+		if(po.prime?)
+			return po
+		end
+	end
+	raise Exception.new('not found (deg='.deg.to_s+')')
+end
+
+def test_get_prime_count(deg)
+	c=0
+	for i in (1<<(deg))...(1<<(deg+1))
+		if GF2Poly.new(i).prime?
+			c+=1
+		end
+	end
+	return c
 end
