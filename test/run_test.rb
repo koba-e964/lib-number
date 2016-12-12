ef5234973accd4550600599691969af2ece6a14a
koba-e964 <koba_max@kobas-Air.pohe>
2016-12-12 17:54:40 +0900

    Add run_test.rb (automatic test runner)

---
+++ test/run_test.rb
@@ -0,0 +1,10 @@
+# Reference: https://test-unit.github.io/test-unit/ja/file.how-to.html
+base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+lib_dir  = base_dir
+test_dir = File.join(base_dir, "test")
+
+$LOAD_PATH.unshift(lib_dir)
+
+require 'test/unit'
+
+exit Test::Unit::AutoRunner.run(true, test_dir)
